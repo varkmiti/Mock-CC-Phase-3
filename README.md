bbbd593bf6062b472a9f115228138d548e0614f7
Mark Viti <+>
2022-07-20 10:11:57 -0400

    All core deliverables done.

--- README.md
+++ README.md
@@ -42,10 +42,10 @@ As a user, I can:
 - See all ramen images in the `div` with the id of `ramen-menu`. When the page
   loads, request the data from the server to get all the ramen objects. Then,
   display the image for each of the ramen using an `img` tag inside the
-  `#ramen-menu` div.
+  `#ramen-menu` div. DONE. 
 - Click on an image from the `#ramen-menu` div and see all the info about that
   ramen displayed inside the `#ramen-detail` div and where it says
-  `insert comment here` and `insert rating here`.
+  `insert comment here` and `insert rating here`. DONE.
 - Create a new ramen after submitting the `new-ramen` form. The new ramen should
   be added to the`#ramen-menu` div. The new ramen does not need to persist; in
   other words, if you refresh the page, it's okay that the new ramen is no
--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
 // write your code here
+URL = "http://localhost:3000/ramens"
+const ramenMenu = document.querySelector("#ramen-menu")
+const ramenDetail = document.querySelector('#ramen-detail')
+const newRamenForm = document.querySelector('#new-ramen')
+
+fetch(URL)
+.then(res => res.json())
+.then(displayRamen)
+
+function displayRamen(ramenArray) {
+  ramenArray.forEach(ramen => displaySingleRamen(ramen))
+
+  function displaySingleRamen(ramen) {
+    const ramenDiv = document.createElement('div')
+  
+    ramenDiv.innerHTML = `<image src=${ramen.image} id = ${ramen.id}>`
+  
+    ramenDiv.addEventListener("click", (event)=>{
+      ramenTargetObject = ramenArray.find(ramen => ramen.id == event.target.id)
+      showRamenInfo(ramenTargetObject)
+    })
+  
+    function showRamenInfo(ramenTargetObject) {
+      ramenDetail.innerHTML = `<div id="ramen-detail">
+                                <img class="detail-image" src=${ramenTargetObject.image} alt=${ramenTargetObject.comment}>
+                                <h2 class="name">${ramenTargetObject.name}</h2>
+                                <h3 class="restaurant">${ramenTargetObject.restaurant}</h3>
+                              </div>`
+      const rating = document.querySelector('#rating-display')
+      rating.innerText = ramenTargetObject.rating
+
+      const comment = document.querySelector('#comment-display')
+      comment.innerText = ramenTargetObject.comment
+    }
+  
+    ramenMenu.appendChild(ramenDiv)
+}
+
+newRamenForm.addEventListener("submit", (event => {
+  console.log(event.target)
+  const newRamenData = {
+    name: event.target.name.value,
+    restaurant: event.target.restaurant.value,
+    image: event.target.image.value,
+    rating: event.target.rating.value,
+    comment: event.target['new-comment'].value
+  }
+  fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify(newRamenData)
+  })
+}))
+
+
+}
+
+
+
